f75221ac280c9adf514e3007341a588d766881eb
humasoft <jorge@humasoft.com>
2018-07-19 19:40:00 +0200

    modificado:    main/isatransactions.cpp

--- main/isatransactions.cpp
+++ main/isatransactions.cpp
@@ -19,7 +19,7 @@ main ()
     double dts=0.01;
 
 
-    //fpd w=100 pm=80
+//    //fpd w=100 pm=80
     vector<double> npd ={   33.3226,  143.5141,  347.1889,  451.1660 , 217.6672};
     vector<double> dpd ={ 0.2520  ,  1.6080  ,  3.4552    ,3.0993   , 1.0000};
 
@@ -28,7 +28,7 @@ main ()
     vector<double> npi ={0.3354  ,  0.3724  , -1.8968 ,  -0.5654  ,  1.9306};
     vector<double> dpi ={0.2381  ,  0.3986  , -1.0645  , -0.5716  ,  1.0000};
 
-//    //fpd w=25 pm=70
+    //fpd w=25 pm=70 //kept from last experiments.
 //    vector<double> npd ={26.1251, -119.3468, -293.8182,  178.9746 , 361.7371};
 //    vector<double> dpd ={-0.1440 , -0.1031 , 1.1445 , 2.1032 , 1.0000};
 
@@ -40,16 +40,22 @@ main ()
 //    vector<double> npd ={16.1209  , -8.1197  ,-92.0813 ,  74.6016  ,172.6901};
 //    vector<double> dpd ={  -0.1151   , 0.0179  ,  1.3106  ,  2.1779   , 1.0000};
 
-    SystemBlock pi1(npi,dpi,1);
-    SystemBlock pi3(npi,dpi,1);
-    SystemBlock pi2(npi,dpi,1);
+    SystemBlock pi1(npi,dpi);
+    SystemBlock pi3(npi,dpi);
+    SystemBlock pi2(npi,dpi);
 
 //    PIDBlock pi1(1,100,0,dts);
 //    PIDBlock pi2(1,100,0,dts);
 //    PIDBlock pi3(1,100,0,dts);
+
+
 //    PIDBlock pd1(34.180445,25.521828,0.8734306,dts);
 //    PIDBlock pd2(34.180445,25.521828,0.8734306,dts);
 //    PIDBlock pd3(34.180445,25.521828,0.8734306,dts);
+
+//    PIDBlock pd1(5,0,0,dts);
+//    PIDBlock pd2(5,0,0,dts);
+//    PIDBlock pd3(5,0,0,dts);
 
     SystemBlock pd1(npd,dpd,1);
     SystemBlock pd2(npd,dpd,1);
@@ -100,7 +106,7 @@ main ()
 //    posan2=(0.1-lengths[1])*180/(0.01*M_PI);
 //    posan3=(0.1-lengths[2])*180/(0.01*M_PI);
 double posan1, posan2, posan3;
-    posan1=50;
+    posan1=80;
     posan2=-posan1/2;
     posan3=-posan1/2;
     cout << "pos1 " << posan1  << ", pos2 " << posan2 << ", pos3 " << posan3 <<endl;
@@ -114,9 +120,10 @@ double posan1, posan2, posan3;
 
     double interval=2; //in seconds
 
-    pd1.SetSaturation(-100,100);
-    pd2.SetSaturation(-100,100);
-    pd3.SetSaturation(-100,100);
+    double sats=80;
+    pd1.SetSaturation(-sats,sats);
+    pd2.SetSaturation(-sats*0.5,sats*0.5);
+    pd3.SetSaturation(-sats*0.5,sats*0.5);
 
 
 
@@ -126,11 +133,11 @@ double posan1, posan2, posan3;
         {
             ep1=posan1-m1.GetPosition();
             ev1= (ep1 > pd1)-m1.GetVelocity();
-            m1.SetTorque(ev1 > pi1);
+            m1.SetTorque((ev1 > pi1));
 
             ep2=posan2-m2.GetPosition();
             ev2= (ep2 > pd2)-m2.GetVelocity();
-            m2.SetTorque(ev2 > pi2);
+            m2.SetTorque((ev2 > pi2));
 
             ep3=posan3-m3.GetPosition();
             ev3= (ep3 > pd3)-m3.GetVelocity();
