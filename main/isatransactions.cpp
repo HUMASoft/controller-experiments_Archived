82f6664bae80b2561d53e15769547d7b8b423ab3
humasoft <jorge@humasoft.com>
2018-07-24 18:00:20 +0200

    modificado:    main/isatransactions.cpp

--- main/isatransactions.cpp
+++ main/isatransactions.cpp
@@ -19,66 +19,56 @@ main ()
     double dts=0.01;
 
 
-////    //fpd w=100 pm=80
-//    vector<double> npd ={   33.3226,  143.5141,  347.1889,  451.1660 , 217.6672};
-//    vector<double> dpd ={ 0.2520  ,  1.6080  ,  3.4552    ,3.0993   , 1.0000};
-
 
     //fpi w=25 pm=70 //kept from last experiments.
     vector<double> npi ={0.3354  ,  0.3724  , -1.8968 ,  -0.5654  ,  1.9306};
     vector<double> dpi ={0.2381  ,  0.3986  , -1.0645  , -0.5716  ,  1.0000};
 
-    //fpd w=25 pm=70 //kept from last experiments.
-//    vector<double> npd ={26.1251, -119.3468, -293.8182,  178.9746 , 361.7371};
-//    vector<double> dpd ={-0.1440 , -0.1031 , 1.1445 , 2.1032 , 1.0000};
-
-//    //w20p50isom scilab
-//    vector<double> npd ={-5310.6413 ,  33607.557 , -52182.099 ,  23885.413};
-//    vector<double> dpd ={0. ,  0.221738 , -1.1842456  , 1.};
-
-
-//    //w15p100isom matlab
-//    vector<double> npd ={  -0.0849 , -18.2048 , -24.0183,   32.3824  , 40.6971};
-//    vector<double> dpd ={ -0.1826  , -0.4825  ,  0.4194   , 1.7166  ,  1.0000};
-
-    //w10p80isom matlab
-//    vector<double> npd ={    9.4033 ,  61.7897 , -96.8906 ,-120.3604 , 177.9012};
-//    vector<double> dpd ={  -0.1189 ,  -0.3611   , 0.4540  ,  1.7011   , 1.0000};
-
-//    //w12p60isom fpi
-//    vector<double> npd ={        0.1903  ,  2.7145  , -3.0096  , -5.2954  ,  5.4174};
-//    vector<double> dpd ={        0.0321  ,  0.5141 ,  -0.5422 ,  -1.0022 ,   1.0000};
-    //or maybe:
-//    vector<double> npd ={0.0544  ,  1.0253  , -0.9935  , -1.9059   , 1.8320};
-//    vector<double> dpd ={0.0273  ,  0.6002 ,  -0.5238 ,  -1.1035  ,  1.0000};
-
-    //w12p60monjes fpi
-    //aproximation of s^-0.8: 5.3825451/(0.0022612 + 2.2612216*s)
-    vector<double> npd ={ 0.0539 ,   1.0203  , -0.9857 ,  -1.8960  ,  1.8194};
-    vector<double> dpd ={0.0273 ,   0.6002  , -0.5238  , -1.1035   , 1.0000};
 
     SystemBlock pi1(npi,dpi);
     SystemBlock pi3(npi,dpi);
     SystemBlock pi2(npi,dpi);
 
-//    PIDBlock pi1(1,100,0,dts);
-//    PIDBlock pi2(1,100,0,dts);
-//    PIDBlock pi3(1,100,0,dts);
-
-
-//    PIDBlock pd1(8.6120054,10.826259,0.2030172,dts);
-//    PIDBlock pd2(8.6120054,10.826259,0.2030172,dts);
-//    PIDBlock pd3(8.6120054,10.826259,0.2030172,dts);
-
-
-    SystemBlock pd1(npd,dpd,1);
-    SystemBlock pd2(npd,dpd,1);
-    SystemBlock pd3(npd,dpd,1);
+    string mass("/home/buyus/Escritorio/600g");
 
 
 
-    ofstream targets ("targets.csv");
-    ofstream responses ("responses.csv");
+    //w12p60isom fpi
+
+//    string method("w12p60isom");
+//    vector<double> npd ={0.1903  ,  2.7145  , -3.0096  , -5.2954  ,  5.4174};
+//    vector<double> dpd ={0.0321  ,  0.5141 ,  -0.5422 ,  -1.0022 ,   1.0000};
+//    SystemBlock pd1(npd,dpd,1);
+//    SystemBlock pd2(npd,dpd,1);
+//    SystemBlock pd3(npd,dpd,1);
+
+
+
+//    w12p60monje fpi
+
+//    string method("w12p60monje");
+//    vector<double> npd ={0.1903  ,  2.7145  , -3.0096  , -5.2854  ,  5.3974};
+//    vector<double> dpd ={0.0321  ,  0.5141 ,  -0.5422 ,  -1.0022 ,   1.0000};
+//    SystemBlock pd1(npd,dpd,1);
+//    SystemBlock pd2(npd,dpd,1);
+//    SystemBlock pd3(npd,dpd,1);
+
+
+
+
+//    //w12p60pid
+
+    string method("w12p60pid");
+    PIDBlock pd1(8.6120054,10.826259,0.2030172,dts);
+    PIDBlock pd2(8.6120054,10.826259,0.2030172,dts);
+    PIDBlock pd3(8.6120054,10.826259,0.2030172,dts);
+
+
+
+    ofstream targets (mass+method+".targets.csv");
+    ofstream responses (mass+method+"0g_responses.csv");
+    ofstream controls (mass+method+"0g_controls.csv");
+
 
 
         SocketCanPort pm1("can0");
@@ -127,10 +117,9 @@ double posan1, posan2, posan3;
 
 
 
-
-    double ep1,ev1;
-    double ep2,ev2;
-    double ep3,ev3;
+    double ep1,ev1,cs1;
+    double ep2,ev2,cs2;
+    double ep3,ev3,cs3;
 
     double interval=2; //in seconds
 
@@ -146,25 +135,30 @@ double posan1, posan2, posan3;
         if (onrobot)
         {
             ep1=posan1-m1.GetPosition();
-            ev1= (ep1 > pd1)-m1.GetVelocity();
+            cs1=ep1 > pd1;
+            ev1= cs1-m1.GetVelocity();
             m1.SetTorque((ev1 > pi1));
 
             ep2=posan2-m2.GetPosition();
-            ev2= (ep2 > pd2)-m2.GetVelocity();
+            cs2=ep2 > pd2;
+            ev2= cs2-m2.GetVelocity();
             m2.SetTorque((ev2 > pi2));
 
             ep3=posan3-m3.GetPosition();
-            ev3= (ep3 > pd3)-m3.GetVelocity();
+            cs3=ep3 > pd3;
+            ev3= cs3-m3.GetVelocity();
             m3.SetTorque(2.1*(ev3 > pi3));
 
-            cout << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
+          //  cout << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
+            controls << t << " , " << cs1 << " , " << cs2 <<  " , " << cs3 <<endl;
             responses << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
+
 //            cout << t << " , " << m1.GetVelocity() << " , " << m2.GetVelocity() <<  " , " << m3.GetVelocity() <<endl;
 //            responses << t << " , " << m1.GetVelocity() << " , " << m2.GetVelocity() <<  " , " << m3.GetVelocity() <<endl;
 
         }
         usleep(dts*1000*1000);
-        cout << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
+       // cout << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
         targets << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
 
     }
@@ -202,6 +196,8 @@ double posan1, posan2, posan3;
 
         sleep(1);
      }
-
+targets.close();
+controls.close();
+responses.close();
 
 }
