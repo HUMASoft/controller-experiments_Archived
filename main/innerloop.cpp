5aa4ad7be191ad374f4c7c8c30cbca178dd96a57
HumaSOFT <humasoft@ing.uc3m.es>
2018-12-19 20:02:22 +0100

    testing

--- main/innerloop.cpp
+++ main/innerloop.cpp
@@ -30,16 +30,16 @@ int main ()
     vector<double> npi ={0.2916 ,  -5.3981 ,   0.2415  ,  5.5243};
     vector<double> dpi ={0.2916 ,  -5.3981  ,  0.2415  ,  5.5243};
 
-    SystemBlock pi1(npi,dpi);
-    SystemBlock pi3(npi,dpi);
-    SystemBlock pi2(npi,dpi);
+//    SystemBlock pi1(npi,dpi);
+//    SystemBlock pi3(npi,dpi);
+//    SystemBlock pi2(npi,dpi);
 //    pi2.SetSaturation(-700,700);
 
 
 //    //string method("w12p60pid");
-//    PIDBlock pi1(8.6120054,10.826259,0.2030172,dts);
-//    PIDBlock pi2(0.165,21.15,0,dts);
-//    PIDBlock pi3(8.6120054,10.826259,0.2030172,dts);
+    PIDBlock pi1(8.6120054,10.826259,0.2030172,dts);
+    PIDBlock pi2(0.165,21.15,0,dts);
+    PIDBlock pi3(8.6120054,10.826259,0.2030172,dts);
 
     string mass("/home/humasoft/Escritorio/");
 
@@ -85,8 +85,7 @@ int main ()
         SocketCanPort pm3("can1");
         CiA402Device m3 (3, &pm3);
 
-        if (onrobot)
-        {
+
 //  Remember to switch on before to keep this commented.
 
 //        m1.Reset();
@@ -103,30 +102,15 @@ int main ()
         m1.Setup_Torque_Mode();
         m2.Setup_Torque_Mode();
         m3.Setup_Torque_Mode();
-    }
-
-//    TableKinematics a;
-//    vector<double> lengths(3);
-//    long orient=1;
-//    long incli=1;
-
-//    a.GetIK(incli,orient,lengths);
-//    cout << "l1 " << lengths[0]  << ", l2 " << lengths[1] << ", l3 " << lengths[2]<<endl;
-//    double posan1, posan2, posan3;
-//    posan1=(0.1-lengths[0])*180/(0.01*M_PI);
-//    posan2=(0.1-lengths[1])*180/(0.01*M_PI);
-//    posan3=(0.1-lengths[2])*180/(0.01*M_PI);
-double posan1, posan2, posan3;
-    posan1=80;
-    posan2=-posan1/2;
-    posan3=-posan1/2;
-    cout << "pos1 " << posan1  << ", pos2 " << posan2 << ", pos3 " << posan3 <<endl;
 
 
 
-    double ep1,ev1,cs1;
-    double ep2,ev2,cs2;
-    double ep3,ev3,cs3;
+
+
+
+    double tg1=50,ev1,cs1;
+    double tg2=0,ev2,cs2;
+    double tg3=0,ev3,cs3;
 
     double interval=2; //in seconds
 
@@ -139,71 +123,41 @@ double posan1, posan2, posan3;
 
     for (double t=0;t<interval; t+=dts)
     {
-        if (onrobot)
-        {
-            ep1=posan1-m1.GetPosition();
-            cs1=ep1 > pd1;
-            ev1= cs1-m1.GetVelocity();
-            ev1=100;
-            m1.SetTorque((ev1 > pi1));
 
-            ep2=posan2-m2.GetPosition();
-            cs2=ep2 > pd2;
+            ev1= tg1-m1.GetVelocity();
+            cs1=pi1.OutputUpdate(ev1);
+            m1.SetTorque(cs1);
+
             ev2= cs2-m2.GetVelocity();
             m2.SetTorque((ev2 > pi2));
 
-            ep3=posan3-m3.GetPosition();
-            cs3=ep3 > pd3;
             ev3= cs3-m3.GetVelocity();
             m3.SetTorque((ev3 > pi3));
 
-          //  cout << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
+//            cout << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
+            cout << t << " , " << cs1 << " , " << cs2 <<  " , " << cs3 <<endl;
+//            cout << t << " , " << ev1 << " , " << ev2 <<  " , " << ev3 <<endl;
+//            cout << t << " , " << m1.GetVelocity() << " , " << m2.GetVelocity() <<  " , " << m3.GetVelocity() <<endl;
+
             controls << t << " , " << cs1 << " , " << cs2 <<  " , " << cs3 <<endl;
             responses << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
 
-//            cout << t << " , " << m1.GetVelocity() << " , " << m2.GetVelocity() <<  " , " << m3.GetVelocity() <<endl;
 //            responses << t << " , " << m1.GetVelocity() << " , " << m2.GetVelocity() <<  " , " << m3.GetVelocity() <<endl;
 
-        }
+
         usleep(uint(dts*1000*1000));
        // cout << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
-        targets << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
+//        targets << t << " , " << posan1  << " , " << posan2 << " , " << posan3 << endl;
 
     }
 
-
-    if (onrobot)
-    {
-        posan1=000;
-        posan2=-posan1/2;
-        posan3=-posan1/2;
-        for (double t=0;t<interval; t+=dts)
-        {
-
-                ep1=posan1-m1.GetPosition();
-                ev1= (ep1 > pd1)-m1.GetVelocity();
-                m1.SetTorque(ev1 > pi1);
-
-                ep2=posan2-m2.GetPosition();
-                ev2= (ep2 > pd2)-m2.GetVelocity();
-                m2.SetTorque(ev2 > pi2);
-
-                ep3=posan3-m3.GetPosition();
-                ev3= (ep3 > pd3)-m3.GetVelocity();
-                m3.SetTorque(2.1*(ev3 > pi3));
-
-
-
-            usleep(uint(dts*1000*1000));
-
-        }
 
         m1.SetTorque(0);
         m2.SetTorque(0);
         m3.SetTorque(0);
 
         sleep(1);
-     }
+
 targets.close();
 controls.close();
 responses.close();
@@ -211,3 +165,4 @@ responses.close();
 return 0;
 
 }
+
