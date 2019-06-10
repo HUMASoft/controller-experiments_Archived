8550255ea67e8f30f069ccd133b05ba6e870989a
munozyanez <munoz.yanez@gmail.com>
2019-06-10 16:18:57 +0200

    modificado:     lib/fcontrol 	modificado:     main/testing.cpp

--- main/testing.cpp
+++ main/testing.cpp
@@ -16,24 +16,37 @@
 int main ()
 {
 
-    OnlineSystemIdentification id;
-    FractionalController1DOF fd(1,0.01);
+    OnlineSystemIdentification id(1,2);
+
+    //FractionalController1DOF fd(1,0.01);
     int N=100;
     double dts=0.01;
     vector<double> in(N,0),out(N,0);
 
     for (int i=0; i<N; i++)
     {
-        in[i]=dts;
+        in[i]=1;
+        out[i]=dts;
 //        cout << in[i] << ", ";
         id.UpdateSystem(in[i],out[i]);
+        cout << id.PrintZTransferFunction(dts) << endl;
+        //cout << id.PrintParamsVector() << endl;
+    }
 
+    for (int i=0; i<N; i++)
+    {
+        in[i]=1;
+        out[i]=1;
+//        cout << in[i] << ", ";
+        id.UpdateSystem(in[i],out[i]);
+        cout << id.PrintZTransferFunction(dts) << endl;
+        //cout << id.PrintParamsVector() << endl;
     }
 
     for (int i=0; i<N; i++)
     {
 //        out[i]=fd.OutputUpdate(in[i]);
-        cout << out[i] << ", ";
+//        cout << out[i] << ", ";
 
     }
 
